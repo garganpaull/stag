6265594006b9f258f8735126619bf731364ae5bb
garganpaull <paull@fmo.com.au>
2022-07-15 12:29:04 +1000

    wip

--- getPrices.py
+++ getPrices.py
@@ -23,20 +23,24 @@ def ETL(tickers):
     period1=int(time.mktime(datetime(PrevYear,CurMth,CurDate,CurHour,CurMin).timetuple()))
     period2=int(time.mktime(datetime(CurYear,CurMth,CurDate,CurHour,CurMin).timetuple()))
 
+    app = xw.App(visible=False)
+    wb = xw.Book('C:\Python FMO\stag\MACD3D_mini.xlsx') 
+        
     
     for i in tickers:
         stock=YahooWebQuery=f"https://query1.finance.yahoo.com/v7/finance/download/{i}?period1={period1}&period2={period2}&interval=1d&events=history&includeAdjustedClose=True"
         df=pd.read_csv(stock)
         df = df.iloc[: , :-2]
+        
+        
         df.sort_values(by=['Date'])
           
         #load workbook
-        app = xw.App(visible=False)
-        wb = xw.Book('C:\Python FMO\stag\MACD3D_mini.xlsx') 
-        ws = wb.sheets('SRN')
+        sheet=i[0:3]
+        ws = wb.sheets(sheet)
 
         #Update workbook at specified range
-        ws.range('B7').options(index=False).value = df
+        ws.range('B7').options(index=False, header=False).value = df
         wb.save()
 
     #Close workbook
@@ -45,7 +49,7 @@ def ETL(tickers):
     app.quit()
         
 
-    print(df)
+    #print(df)
 
 
 
