45833ffe3bdbd383dc93568c8d357ca916f526b5
garganpaull <paull@fmo.com.au>
2022-07-23 16:19:34 +1000

    utils

---
+++ utils.py
@@ -0,0 +1,31 @@
+import xlwings as xw
+import tickers,workbook
+
+
+def copyCols(tickers, file, cols):
+#cop cols from MODEL
+#paste to all other ticker sheets
+
+
+
+    with xw.App(visible=False) as app:
+        #Load Excel Workbook
+        wb = xw.Book(file)
+        
+        try:
+            for i in tickers:
+                #copy cols from MODEL sheet
+                active_sheet= wb.sheets['1_MODEL']
+                active_sheet.range(cols).copy(wb.sheets[i[0:3]].range(cols))
+                active_sheet.autofit(axis="columns")
+                print(i[0:3])
+        except:
+            print(f'Error sourcing prices for ticker {i}')
+
+        wb.save()
+    return 'Process Finished'
+        
+file=workbook.getModelFile()
+tickers=tickers.getTickers(file)
+cols='X6:Y668'
+copyCols(tickers,file, cols)    
